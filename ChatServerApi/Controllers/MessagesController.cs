549c0d87c94eb414f50bbcd255e895538e77f9ef
vovsemenv <vovsemenv@gmail.com>
2020-02-17 00:35:14 +0300

    Add project files.

---
+++ ChatServerApi/Controllers/MessagesController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using Newtonsoft.Json;
+
+namespace ChatServerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+            private ConnectionMultiplexer redis;
+            IDatabase db;
+
+            public MessagesController()
+            {
+                redis = ConnectionMultiplexer.Connect("localhost");
+                db = redis.GetDatabase();
+
+            }
+
+            //GET: api/Messages
+            [HttpGet]
+            public IEnumerable<Message> Get()
+            {
+
+                var res = db.Execute("get", "messages");
+                var arr = new List<Message>();
+                if (res.IsNull)
+                {
+                    return new List<Message>();
+                }
+                else
+                {
+                    var deser = JsonConvert.DeserializeObject<List<Message>>(res.ToString());
+
+                    return deser;
+                }
+
+            }
+
+
+            // POST: api/Messages
+            [HttpPost]
+            public void Post([FromBody] Message value)
+            {
+
+
+
+                var res = db.Execute("get", "messages");
+                if (res.IsNull)
+                {
+                    var arr = new List<Message>();
+
+                    arr.Add(new Message(value.message, value.name));
+                    var jsst = JsonConvert.SerializeObject(arr);
+                    db.Execute("set", new object[] { "messages", jsst });
+                }
+                else
+                {
+
+                    var deser = JsonConvert.DeserializeObject<List<Message>>(res.ToString());
+                    deser.Add(new Message(value.message, value.name));
+
+                    var jsst = JsonConvert.SerializeObject(deser);
+                    db.Execute("set", new object[] { "messages", jsst });
+
+                }
+            }
+
+        }
+}
---
+++ ChatServerApi/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatServerApi
+{
+    public class Message
+    {
+        public string name { get; set; }
+        public string message { get; set; }
+        public Message(string message, string username)
+        {
+            this.message = message;
+            this.name = username;
+        }
+        public Message()
+        {
+            this.message = "";
+            this.name = "";
+        }
+    }
+}
