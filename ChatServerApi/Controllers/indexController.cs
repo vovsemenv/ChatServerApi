4870dd3cb8ee415113a9639cf9c65c14fe8f3ef9
vovsemenv <vovsemenv@gmail.com>
2020-02-17 21:10:09 +0300

    add index for check site status

---
+++ ChatServerApi/Controllers/indexController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatServerApi.Controllers
+{
+    [Route("")]
+    public class indexController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {            
+            return new string[] { "for get data for chat use  GET chatserverapi.azurewebsites.net/api/messages",
+                                  "for send data for chat use  POST chatserverapi.azurewebsites.net/api/messages with body {'message':'messageHere','name':'nameHere'}"
+            };
+        }
+
+    }
+}
